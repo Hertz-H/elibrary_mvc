dcc78a207b951d41ec85032a3fcc790a3f600354
Hertz-H <afaf14albakri@gmail.com>
2022-03-21 18:01:44 +0300

    with data builder

--- app/modules/book.php
+++ app/modules/book.php
@@ -1,8 +1,17 @@
 <?php
 
 class book{
-    function select(){
+  private $pdo;
+  function __construct()
+    {
+     
+       
+      require_once('DBClass.php');
+      $this->pdo=new DatabaseClass();
 
+    }
+    function select(){
+      $books_result = $this->pdo->select()->query_build();
       //query builder call;
      return  $books_result;
     }
